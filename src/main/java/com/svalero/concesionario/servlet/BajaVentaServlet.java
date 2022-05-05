5f477b55b41d63bed71cc96eba21e96ba5299a04
Antonio de la Rosa <santiagodelarosaantonio@gmail.com>
2022-05-05 12:47:33 +0200

    Fix: Bug #12 corregido

--- src/main/java/com/svalero/concesionario/servlet/BajaVentaServlet.java
+++ src/main/java/com/svalero/concesionario/servlet/BajaVentaServlet.java
@@ -1,7 +1,9 @@
 package com.svalero.concesionario.servlet;
 
+import com.svalero.concesionario.dao.ComplementosDAO;
 import com.svalero.concesionario.dao.Database;
 import com.svalero.concesionario.dao.VentaDAO;
+import com.svalero.concesionario.domain.Complementos;
 import com.svalero.concesionario.util.ValidaSesion;
 
 import javax.servlet.RequestDispatcher;
@@ -16,6 +18,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 @WebServlet("/bajaVenta")
 public class BajaVentaServlet extends HttpServlet {
@@ -31,6 +34,7 @@ public class BajaVentaServlet extends HttpServlet {
         String contrasenia = (String) sesion.getAttribute("password");
 
         String id = req.getParameter("id");
+        System.out.println("Parametro eliminar conseguido: " + id);
 
         if(nombreUsuario == null || contrasenia == null || !ValidaSesion.validar(nombreUsuario, contrasenia)){
             RequestDispatcher dispatcher = req.getRequestDispatcher("login.jsp");
@@ -38,18 +42,40 @@ public class BajaVentaServlet extends HttpServlet {
         } else {
             Database database = new Database();
             Connection connection = database.getConnection();
-            boolean correcto;
+            boolean correctoVenta;
+            boolean correctoComplementos;
 
             try {
                 VentaDAO ventaDAO = new VentaDAO(connection);
-                correcto = ventaDAO.borraVenta(Integer.parseInt(id));
-                if(correcto){
+                ComplementosDAO complementosDAO = new ComplementosDAO(connection);
+
+                ArrayList<Complementos> complementos = complementosDAO.findComplemntoVenta(id);
+
+                connection.setAutoCommit(false);
+
+                if(!complementos.isEmpty()){
+                    correctoComplementos = complementosDAO.borraComplementos(id);
+                    if(correctoComplementos){
+                        out.println("<div class='alert alert-success' role='alert'>Complementos eliminados correctamente!</div>");
+                    } else {
+                        out.println("<div class='alert alert-danger' role='alert'>No hay complementos que eliminar</div>");
+                    }
+                }
+
+                correctoVenta = ventaDAO.borraVenta(Integer.parseInt(id));
+                if(correctoVenta){
                     out.println("<div class='alert alert-success' role='alert'>Venta eliminada correctamente!</div>");
+                    connection.commit();
                 } else {
                     out.println("<div class='alert alert-danger' role='alert'>No se ha podido eliminar la venta</div>");
+                    connection.rollback();
                 }
+                connection.setAutoCommit(true);
             } catch (SQLException sqle) {
                 out.println("<div class='alert alert-danger' role='alert'>Ha habido un error con la base de datos</div>");
+                System.out.println(sqle);
+            } finally {
+                database.close();
             }
         }
     }
